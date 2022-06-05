bd6fb1115b23d3e6f252a2c25956af0f90f3c467
andarutr <andarutr@gmail.com>
2022-06-06 06:25:31 +0700

    Telah menyelesaikan Tugas Pertemuan 1: Melakukan git dan menginstall beserta membuat controller pada Framework Codeigniter

---
+++ kamus-kucing/application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "<p>Nama saya Andaru Triadi, berasal dari
+		Ponorogo, Jawa Timur dan menetap di Jakarta. Hobi
+		saya membaca buku.</p>";
+	}
+}
